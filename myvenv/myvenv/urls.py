576a3e7f47ced6c5a07fc613a7417e49651186ef
zainub qadir <zainubq16@gmail.com>
2021-02-07 00:52:33 -0500

    Jobs app up and running

--- myvenv/myvenv/urls.py
+++ myvenv/myvenv/urls.py
@@ -1,4 +1,4 @@
-"""portfolio URL Configuration
+"""myvenv URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
